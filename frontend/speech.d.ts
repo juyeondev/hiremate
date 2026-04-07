91dd05ce9ffb849fdc06e6fe3b4cd42bc4bf18ef
Jay <jay@Jayui-MacBook.local>
2026-04-07 10:16:31 +0900

    [fix]Fix Build error - speech.d.ts

--- frontend/speech.d.ts
+++ frontend/speech.d.ts
@@ -2,6 +2,10 @@ interface SpeechRecognitionEvent extends Event {
   results: SpeechRecognitionResultList;
 }
 
+interface SpeechRecognitionErrorEvent extends Event {
+  error: string;
+}
+
 interface Window {
   SpeechRecognition: new () => SpeechRecognition;
   webkitSpeechRecognition: new () => SpeechRecognition;
